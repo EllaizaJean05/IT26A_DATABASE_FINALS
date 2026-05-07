f2a6d2f24eb6b3710f99d5055eadfa7ff9bf1426
Ellaiza Jean Balatero <ejbalatero05@gmail.com>
2026-05-07 22:39:19 +0800

    added db.php and login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CampusSafe Login</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="login-container">
+
+    <h2>CampusSafe Login</h2>
+
+    <form action="login_process.php" method="POST">
+
+        <input type="email" name="email" placeholder="Email" required>
+
+        <input type="password" name="password" placeholder="Password" required>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+</div>
+
+<script src="auth.js"></script>
+
+</body>
+</html>
